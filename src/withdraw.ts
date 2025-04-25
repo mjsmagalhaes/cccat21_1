20e06d1a5b7405a7a5c5b0bd00236409bb0ee390
Marcos Magalhaes <mjsmagalhaes.insc@gmail.com>
2025-04-24 21:34:45 -0300

    CDU Withdraw

--- src/withdraw.ts
+++ src/withdraw.ts
@@ -10,56 +10,56 @@ const debug = Debug("withdraw");
 Debug.enable("withdraw, db, error");
 
 router.post("/", async (req: Request, res: Response) => {
-  const input = req.body;
-  const db = new Database();
-  const quantity = parseFloat(input.quantity);
+    const input = req.body;
+    const db = new Database();
+    const quantity = parseFloat(input.quantity);
 
-  const account = await db.getAccount(input.accountId);
-  if (account == null) {
-    return res.status(422).json({
-      error: "Account not found.",
+    const account = await db.getAccount(input.accountId);
+    if (account == null) {
+        return res.status(422).json({
+            error: "Account not found.",
+        });
+    }
+
+    const asset = await db.getAsset(input.assetId);
+    if (asset == null) {
+        return res.status(422).json({
+            error: "Asset not found.",
+        });
+    }
+
+    if (!input.quantity || isNaN(quantity) || quantity < 0) {
+        return res.status(422).json({
+            error: "Wrong quantity.",
+        });
+    }
+
+    const wallet = await db.getWallet(account.account_id, asset.asset_id);
+    if (wallet == null) {
+        return res.status(422).json({
+            error: "Wallet not found.",
+        });
+    }
+
+    if (wallet.quantity < quantity) {
+        return res.status(422).json({
+            error: "Insufficient funds.",
+        });
+    }
+
+    debug(`Wallet ${account.account_id}:${asset.asset_id} found. Updating ...`);
+    await db.updateWallet(
+        account.account_id,
+        asset.asset_id,
+        wallet.quantity - quantity
+    );
+
+    const new_wallet = await db.getWallet(account.account_id, asset.asset_id);
+    res.json({
+        status: "ok",
+        quantity: new_wallet.quantity,
+        asset_id: new_wallet.asset_id,
     });
-  }
-
-  const asset = await db.getAsset(input.assetId);
-  if (asset == null) {
-    return res.status(422).json({
-      error: "Asset not found.",
-    });
-  }
-
-  if (!input.quantity || isNaN(quantity) || quantity < 0) {
-    return res.status(422).json({
-      error: "Wrong quantity.",
-    });
-  }
-
-  const wallet = await db.getWallet(account.account_id, asset.asset_id);
-  if (wallet == null) {
-    return res.status(422).json({
-      error: "Wallet not found.",
-    });
-  }
-
-  if (wallet.quantity < quantity) {
-    return res.status(422).json({
-      error: "Insufficient funds.",
-    });
-  }
-
-  debug(`Wallet ${account.account_id}:${asset.asset_id} found. Updating ...`);
-  await db.updateWallet(
-    account.account_id,
-    asset.asset_id,
-    quantity + wallet.quantity
-  );
-
-  const new_wallet = await db.getWallet(account.account_id, asset.asset_id);
-  res.json({
-    status: "ok",
-    quantity: new_wallet.quantity,
-    asset_id: new_wallet.asset_id,
-  });
 });
 
 app.use("/withdraw", router);
---
+++ test/withdraw.test.ts
@@ -0,0 +1,60 @@
+import axios from "axios";
+
+axios.defaults.validateStatus = () => true;
+
+beforeAll(async () => { });
+
+afterAll(async () => { });
+
+test("Não deve retirar se a conta não existir", async () => {
+    const responseDeposit = await axios.post("http://localhost:3000/withdraw", {
+        accountId: "123",
+    });
+
+    const outputWithdraw = responseDeposit.data;
+    expect(outputWithdraw.error).toBe("Account not found.");
+});
+
+test("Não deve retirar se o asset não existir", async () => {
+    const responseDeposit = await axios.post("http://localhost:3000/withdraw", {
+        accountId: "1fb6e901-f4de-4653-80e7-07c207073f61",
+        assetId: "",
+    });
+
+    const outputWithdraw = responseDeposit.data;
+    expect(outputWithdraw.error).toBe("Asset not found.");
+});
+
+test("Não deve retirar se a quantidade for menor que zero", async () => {
+    const responseDeposit = await axios.post("http://localhost:3000/withdraw", {
+        accountId: "1fb6e901-f4de-4653-80e7-07c207073f61",
+        assetId: "BTC",
+        quantity: 0,
+    });
+
+    const outputWithdraw = responseDeposit.data;
+    expect(outputWithdraw.error).toBe("Wrong quantity.");
+});
+
+test("Não deve retirar se a quantidade for maior que o disponível", async () => {
+    const responseDeposit = await axios.post("http://localhost:3000/withdraw", {
+        accountId: "1fb6e901-f4de-4653-80e7-07c207073f61",
+        assetId: "BTC",
+        quantity: 120,
+    });
+
+    const outputWithdraw = responseDeposit.data;
+    expect(outputWithdraw.error).toBe("Insufficient funds.");
+});
+
+test("Deve fazer uma retirada", async () => {
+    const responseDeposit = await axios.post("http://localhost:3000/withdraw", {
+        accountId: "1fb6e901-f4de-4653-80e7-07c207073f61",
+        assetId: "BTC",
+        quantity: 100,
+    });
+
+    const outputWithdraw = responseDeposit.data;
+    console.log(outputWithdraw);
+    expect(outputWithdraw.status).toBe("ok");
+});
